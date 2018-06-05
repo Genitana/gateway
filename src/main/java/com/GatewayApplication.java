7307b4974a0508693ddd6a65e75523a2dd6b5e3f
robert <1426218266@qq.com>
2018-06-05 21:59:54 +0800

    添加@EnableZuulProxy注解

--- src/main/java/com/GatewayApplication.java
+++ src/main/java/com/GatewayApplication.java
@@ -2,9 +2,13 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
-@EnableZuulServer
+//@EnableZuulServer 这个注解不起作用
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
